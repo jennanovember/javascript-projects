89ee495c77b75c5c256d950199654eefd480511c
Jenna November Borton <142618268+jennanovember@users.noreply.github.com>
2023-10-04 18:28:12 -0500

    finished string exercise

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,9 +1,15 @@
-let num = 1001;
+let num = 123.45;
 
 //Returns 'undefined'.
-console.log(num.length);
+
 
 //Use type conversion to print the length (number of digits) of an integer.
+
+if (String(num).includes('.')){
+    console.log(String(num).length-1);
+    } else {
+    console.log(String(num).length);
+    }
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
 
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -1,11 +1,15 @@
 //Part Three section one
 
 let language = 'JavaScript';
+let initials = 'JS';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
 
+console.log(language.slice(0,1)+language.slice(4,5));
+
 //2. Without using slice(), use method chaining to accomplish the same thing.
 
+console.log(`The abbreviation for '${language}' is '${initials}'.`)
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
 
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
@@ -15,3 +19,10 @@ let language = 'JavaScript';
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+
+let titleCase = notTitleCase
+  .split(' ')
+  .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+  .join(' ');
+
+console.log(titleCase); // Output: "Title Case"
